725f2d4d802ebfff672d4195b1b910c58d155f76
Visibyte <nathanglass26@gmail.com>
2024-08-05 18:49:47 +0100

    drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, type Config } from "drizzle-kit";
+
+const DATABASE_URL = process.env.DATABASE_URL ?? "";
+
+const drizzleConfig = {
+  schema: "./src/drizzle/schema.ts",
+  out: "./migrations",
+  dialect: "postgresql",
+  dbCredentials: { url: DATABASE_URL },
+} satisfies Config;
+
+export default defineConfig(drizzleConfig)
